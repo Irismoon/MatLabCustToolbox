fe48ae1f3b30aec55eef7259b3fcaf7a17ce0a84
Irismoon Wang <1121490236@qq.com>
2019-11-25 12:06:48 +0800

    'update'

--- writing/journalTitleCheck.m
+++ writing/journalTitleCheck.m
@@ -1,15 +1,23 @@
 function [outputArg1,outputArg2] = journalTitleCheck()
 %make journal title consistent , i.e. uppercase the first letter
-fid = fopen(fullfile(env.get('code'),'MatLabCustToolbox','writing','ref.txt'));
-while ~feof(fid)
-    line = fgetl(fid);
-    if ~isempty(regexp(line,'journal', 'once'))
-        y = strsplit(line,{'=','{','}'});
-        [~,id] = max(cellfun(@length,y));
-        y{id} = upperFirst(y{id},' ','robust');
-        line = ['journal = {' y{id} '},'];%how to renew a line in textfile
+fidr = fopen(fullfile(env.get('code'),'MatLabCustToolbox','writing','ref.txt'));
+fidw = fopen(fullfile(env.get('code'),'MatLabCustToolbox','writing','nref.txt'),'w');
+while ~feof(fidr)
+    line = fgetl(fidr);
+%     if ~isempty(regexp(line,'journal', 'once'))
+%         y = strsplit(line,{'=','{','}'});
+%         [~,id] = max(cellfun(@length,y));
+%         y{id} = upperFirst(y{id},' ','robust');
+%         line = ['journal = {' y{id} '},'];%how to renew a line in textfile
+    if ~isempty(regexp(line,'title', 'once'))
+%         y = strsplit(line,'=');
+        y = regexp(line,'{','once');
+        y = line(y+1:end);
+        title = [upper(y(1)) lower(y(2:end))];
+        line = [' title = {' title];
     end
+    fprintf(fidw,sprintf([line '\n']));
 end
-fclose(fid);
+fclose('all');
 end
-
+%Note: you still need to fine tune the 
