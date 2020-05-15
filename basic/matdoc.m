1742c79bb0e7804ac5baba8503d9f88302aa7a9d
Mango <1121490236@qq.com>
2020-05-14 21:33:42 -0700

    [new feature][allow the code version to be recorded which generates the data files]

--- basic/matdoc.m
+++ basic/matdoc.m
@@ -2,10 +2,14 @@ function S = matdoc(varargin)
 %S = matdoc(varargin)
 %this function is to document the matfile, i.e. document the parameters for
 %the file like from which codefile the matfile is generated, date and time,
+%git info(to know the code version when this dataset is generated.)
 %and so on
+%the varargin is recommended to record here, too, since it sometimes also contains necessary.
+%parameters
 S.time = datetime('now');
-st = dbstack('-completenames',1);%omit first frame
+% st = dbstack('-completenames',1);%omit first frame
 S.codefile = st(1).file;
+S.gitInfo = getGitInfo;
 len = length(varargin);
 idx = 1:len;
 if mod(len,2)==0
